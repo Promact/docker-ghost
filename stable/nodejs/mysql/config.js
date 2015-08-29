763463282da5f9bffcfc56a428bf14420854ee91
Chintan Shah <chintanshah@promactinfo.com>
2015-08-30 03:23:20 +0530

    Initial commit

---
+++ stable/nodejs/mysql/config.js
@@ -0,0 +1,33 @@
+// # Ghost Configuration
+// Setup your Ghost install for various [environments](http://support.ghost.org/config/#about-environments).
+
+// Ghost runs in `development` mode by default. Full documentation can be found at http://support.ghost.org/config/
+
+var path = require('path'),
+    config;
+
+config = {
+    // ### Production
+    // When running Ghost in the wild, use the production environment.
+    // Configure your URL and mail settings here
+    production: {
+        url: 'http://ghostcms-latest.cloudapp.net',
+        mail: {},
+        database: {
+            client: 'mysql',
+            connection: {
+                host     : '127.0.0.1',
+                user     : 'ghost',
+                password : '',
+                database : 'ghost',
+                charset  : 'utf8'
+            }
+        },
+        server: {
+            host: '0.0.0.0',
+            port: '2368'
+        }
+    }    
+};
+
+module.exports = config;
